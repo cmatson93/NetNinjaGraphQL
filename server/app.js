aa65755eb5d96961dbe85dff018f13ceb98fe80c
Christina Matson <cmatson93@gmail.com>
2018-11-08 13:56:29 -0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Connect to mlab database
+mongoose.connect(
+  "mongodb://christina:test123@ds115035.mlab.com:15035/gql-ninja"
+);
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log("Now listening for requests on port 4000");
+})
